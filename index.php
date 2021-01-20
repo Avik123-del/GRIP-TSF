f318308d313fe1caba343aa9a383b49a6389b1c2
Avik123-del <61191261+Avik123-del@users.noreply.github.com>
2021-01-21 00:07:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>phpuh</title>
+</head>
+<body>
+    <?php
+            //single line comment
+            #single line comment
+            /*Multi line
+            comment*/
+            // echo "Hello World";
+            echo "this";
+            echo date("h:i:s");
+    ?>
+</body>
+</html>
